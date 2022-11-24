2cb4f1e3464d2432800db4f31c7296e2a1a7c072
Daniarbekov <daniarbekov@gmail.com>
2022-11-25 00:53:57 +0600

    Profile Detail
    добавил страницу профиля
    добавил ссылки

--- instagram/accounts/views.py
+++ instagram/accounts/views.py
@@ -1,7 +1,7 @@
-
+from django.contrib.auth import get_user_model
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import redirect
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, DetailView
 from .forms import CustomUserCreationForm, LoginForm
 
 
@@ -46,3 +46,14 @@ class LoginView(TemplateView):
 def logout_view(request):
     logout(request)
     return redirect('index')
+
+
+class ProfileView(DetailView):
+    model = get_user_model()
+    template_name = 'profile_detail.html'
+    context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        posts = self.object.posts.order_by('-created_at')
+        kwargs['posts'] = posts
+        return super().get_context_data(**kwargs)
