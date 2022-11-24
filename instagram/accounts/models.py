ab53186c3c66545ddcfb08a19cb726ef52a4130e
Daniarbekov <daniarbekov@gmail.com>
2022-11-24 16:50:23 +0600

    Profile, Post, Comment
    добавил модели Profile, Post, Comment

---
+++ instagram/accounts/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+
+class Profile(AbstractUser):
+    email = models.EmailField(verbose_name='Электронная почта', unique=True, blank=True)
+    avatar = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to='avatars',
+        verbose_name='Аватар'
+    )
+    birthday = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Дата рождения'
+    )
+    liked_posts = models.ManyToManyField(verbose_name='Понравившиеся публикации', to='posts.Post', related_name='user_likes')
+    subscriptions = models.ManyToManyField(verbose_name='Подписки', to='accounts.Profile', related_name='subscribers')
+    commented_posts = models.ManyToManyField(verbose_name='Прокомментированные публикации', to='posts.Post', related_name='user_comments')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
--- instagram/core/settings.py
+++ instagram/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     "django_bootstrap5",
+    'accounts',
+    'posts'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'accounts.Profile'
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_URL = '/uploads/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
